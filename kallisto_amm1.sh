0ce741a96288162d597d886896418aa923b8913c
Wen-Juan Ma <wenjuanma84@gmail.com>
2017-02-22 10:27:13 +0100

    add kallisto_amm1.sh

---
+++ kallisto_amm1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#BSUB -L /bin/bash
+#BSUB -o logfile_amm1.out
+#BUSB -e logfile_amm1.err
+#BSUB -u wenjuan.ma@unil.ch
+#BSUB -N
+#BSUB -n 10
+#BSUB -R "span[ptile=10]"
+#BSUB -R "rusage[mem=50000]"
+#BSUB -M 50000000
+#BSUB -J kallistoamm1.sh
+
+#loading necessary modules and databases
+module add UHTS/Analysis/kallisto/0.43.0
+
+for f in Am*_pairedR1.fastq
+do
+kallisto quant -i /scratch/beegfs/monthly/wjma/rtRNAseq/all/ref_tseries/rt_amm_devonly.idx -o ./${f%%_pairedR1.fastq} -b 100 ${f%%_pairedR1.fastq}_pairedR1.fastq ${f%%_pairedR1.fastq}_pairedR2.fastq
+done
+
