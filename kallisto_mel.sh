0f9c3597b23fd3e8d085e8e0def7645677821ef1
Wen-Juan Ma <wjma@dee-serv04.vital-it.ch>
2017-03-01 11:29:18 +0000

    add kallisto_amm1.sh

--- kallisto_mel.sh
+++ kallisto_mel.sh
@@ -14,8 +14,8 @@
 #loading necessary modules and databases
 module add UHTS/Analysis/kallisto/0.43.0
 
-for f in Am*_pairedR1.fastq
+for f in A10*_R1_pair.fastq
 do
-kallisto quant -i /scratch/beegfs/weekly/wjma/Amm_RNAseq/rt_amm_all.idx -o ./${f%%_pairedR1.fastq} -b 100 ${f%%_pairedR1.fastq}_pairedR1.fastq ${f%%_pairedR1.fastq}_pairedR2.fastq
+kallisto quant -i /scratch/beegfs/weekly/wjma/Amm_RNAseq/rt_amm_all.idx -o ./${f%%_R1_pair.fastq} -b 100 ${f%%_R1_pair.fastq}_R1_pair.fastq ${f%%_R1_pair.fastq}_R2_pair.fastq
 done
 
