e4f07b2d0a90bc5e68440cbf9112a62d4ab72d1b
Wen-Juan Ma <wenjuanma84@gmail.com>
2017-12-03 17:35:41 +0100

    add .

---
+++ script/transdecode_xtro.sh
@@ -0,0 +1,3 @@
+module add UHTS/Assembler/TransDecoder/2.0.1
+
+TransDecoder.Predict -t Xtropicalisv9.0.Named.primaryTrs.fa 
---
+++ script/transdocode_amm.sh
@@ -0,0 +1,3 @@
+module add UHTS/Assembler/TransDecoder/2.0.1
+
+TransDecoder.Predict -t AmmMT_HIMin1.fasta.transdecoder.LI2.cds 
