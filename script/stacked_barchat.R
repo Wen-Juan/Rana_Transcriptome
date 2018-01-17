50412efe3bfc3e86d1d049eab1668a9810647651
Wen-Juan Ma <wenjuanma84@gmail.com>
2018-01-17 15:41:40 +0100

    add .

--- script/stacked_barchat.R
+++ script/stacked_barchat.R
@@ -20,12 +20,16 @@ head(sb)
 blues <- RColorBrewer::brewer.pal(4, "Blues")
 reds <- RColorBrewer::brewer.pal(4, "Reds")
 
+pdf("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/output/figures/sex_bias_alltissues.pdf", height=10, width=10)
 ggplot(data = sb, aes(x=Sex_bias, y= Gene_number, fill=interaction(factor(Fold_change),Sex_bias), label=Gene_number)) + 
   geom_bar(stat="identity") +
   scale_fill_manual(values = c(reds,blues), name="Fold change",labels=c("FDR ≤0.05","Log2 ≥1","Log2 ≥2","Log2 ≥3","FDR ≤0.05","Log2 ≥1","Log2 ≥2","Log2 ≥3")) +
   facet_grid(~Gosner_stage) +
   geom_text(size = 4, position = position_stack(vjust = 0.5)) +
-  scale_x_discrete(labels=c("female", "male"),name="Sex bias") +
-  scale_y_continuous(name = "Number of genes", limits = c(0,7000)) + 
+  scale_x_discrete(labels=c("XX", "XY"),name="Sex bias") +
+  scale_y_continuous(name = "Number of genes", limits = c(0,6500)) + 
   theme(axis.title.x = element_text(size=16,colour = "black"),axis.title.y = element_text(size=16,colour = "black")) +
   theme(axis.text.x = element_text(colour="black",size=11),axis.text.y = element_text(colour="black",size=11))
+
+dev.off()
+
