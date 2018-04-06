ec36cbffbf08cd3a0081a004be12402158e095f6
Wen-Juan Ma <wenjuanma84@gmail.com>
2018-04-06 00:04:07 -0400

    add .

--- script/RKPM.R
+++ script/RKPM.R
@@ -174,48 +174,80 @@ kdata1 <- subset(kdata, kdata$logFC.XYtestis.XXovary<=-1)
 kdata2 <- subset(kdata, kdata$logFC.XYtestis.XXovary>=1)
 kdata3 <- subset(kdata, kdata$logFC.XYtestis.XXovary>-1 & kdata$logFC.XYtestis.XXovary<1)
 
-kdata1$meanXY <- (log2(kdata1$A15MT1) + log2(kdata1$A17MT1) + log2(kdata1$A6MT1) + log2(kdata1$A8MT1) + log2(kdata1$A12MT1)) / 5 
+kdata1$meanXY <- (kdata1$logA15MT1 + kdata1$logA17MT1 + kdata1$logA6MT1 + kdata1$logA8MT1 + kdata1$logA12MT1) /5 
 kdata1$group1="meanmale"
 
-kdata2$meanXY <- (log2(kdata2$A15MT1) + log2(kdata2$A17MT1) + log2(kdata2$A6MT1) + log2(kdata2$A8MT1) + log2(kdata2$A12MT1)) / 5 #XY male group at gonad
+kdata2$meanXY <- (kdata2$logA15MT1 + kdata2$logA17MT1 + kdata2$logA6MT1 + kdata2$logA8MT1 + kdata2$logA12MT1) / 5 
 kdata2$group1="meanmale"
 
-kdata3$meanXY <- (log2(kdata3$A15MT1) + log2(kdata3$A17MT1) + log2(kdata3$A6MT1) + log2(kdata3$A8MT1) + log2(kdata3$A12MT1)) / 5
+kdata3$meanXY <- (kdata3$logA15MT1 + kdata3$logA17MT1 + kdata3$logA6MT1 + kdata3$logA8MT1 + kdata3$logA12MT1) /5
 kdata3$group1="meanmale"
 
-kdata1$meanXX <- (log2(kdata1$A10FO1) + log2(kdata1$A17FO1) + log2(kdata1$A2FO2) + log2(kdata1$A6FO1) + log2(kdata1$A8FO2)) / 5 
+kdata1$meanXX <- (kdata1$logA10FO1 + kdata1$logA17FO1 + kdata1$logA2FO2 + kdata1$logA6FO1 + kdata1$logA8FO2) / 5 
 kdata1$group2="meanfemale"
 
-kdata2$meanXX <- (log2(kdata2$A10FO1) + log2(kdata2$A17FO1) + log2(kdata2$A2FO2) + log2(kdata2$A6FO1) + log2(kdata2$A8FO2)) #XX female group at gonad
+kdata2$meanXX <- (kdata2$logA10FO1 + kdata2$logA17FO1 + kdata2$logA2FO2 + kdata2$logA6FO1 + kdata2$logA8FO2) / 5 #XX female group at gonad
 kdata2$group2="meanfemale"
 
-kdata3$meanXX <- (log2(kdata3$A10FO1) + log2(kdata3$A17FO1) + log2(kdata3$A2FO2) + log2(kdata3$A6FO1) + log2(kdata3$A8FO2)) 
+kdata3$meanXX <- (kdata3$logA10FO1 + kdata3$logA17FO1 + kdata3$logA2FO2 + kdata3$logA6FO1 + kdata3$logA8FO2) / 5
 kdata3$group2="meanfemale"
 
 write.table(kdata1, "/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_fbias.txt", sep="\t", col.names=F)
 write.table(kdata2, "/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_mbias.txt", sep="\t", col.names=F)
 write.table(kdata3, "/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_unbias.txt", sep="\t", col.names=F)
 
-gonad_fbias_all <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_fbias.txt", header=TRUE)
+gonad_fbias_all <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_fbias2.txt", header=TRUE)
 gonad_fbias_all$bias = "female"
-gonad_mbias_all <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_mbias.txt", header=TRUE)
+gonad_mbias_all <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_mbias2.txt", header=TRUE)
 gonad_mbias_all$bias = "male"
-gonad_unbias_all <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_unbias.txt", header=TRUE)
+gonad_unbias_all <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_unbias2.txt", header=TRUE)
 gonad_unbias_all$bias = "unbias"
 
-write.table(gonad_fbias_all, "/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_fbias_all2.txt", sep="\t", col.names=F)
-write.table(gonad_mbias_all, "/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_mbias_all2.txt", sep="\t", col.names=F)
-write.table(gonad_unbias_all, "/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_unbias_all2.txt", sep="\t", col.names=F)
+write.table(gonad_fbias_all, "/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_fbias_all.txt", sep="\t", col.names=F)
+write.table(gonad_mbias_all, "/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_mbias_all.txt", sep="\t", col.names=F)
+write.table(gonad_unbias_all, "/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_unbias_all.txt", sep="\t", col.names=F)
 
-gonad_bias_all_rpkm <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_rpkm_all_mean.txt", header=TRUE)
-str(gonad_bias_all_rpkm)
+gonad_fbias_rpkm <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_fbias_all.txt", header = TRUE)
+str(gonad_fbias_rpkm)
 
-pdf(file="/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/output/figures/sexbias_logRPKM_gonad.pdf")
-
-ggplot(data = gonad_bias_all_rpkm, aes(x=bias, y= Log2RPKM, fill=sex)) + 
+ggplot(data = gonad_fbias_rpkm, aes(x=Sex, y= log2RPKM, fill=Sex)) + 
   geom_boxplot(notch=TRUE)  +
+  ylim(-2,5) +
   scale_fill_manual(values = c("red","blue")) +
   theme(axis.title.x = element_text(size=15,colour = "black"),axis.title.y = element_text(size=15,colour = "black")) +
   theme(axis.text.x = element_text(colour="black",size=12),axis.text.y = element_text(colour="black",size=12))
 
+gonad_mbias_rpkm <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_mbias_all.txt", header = TRUE)
+str(gonad_mbias_rpkm)
+
+ggplot(data = gonad_mbias_rpkm, aes(x=Sex, y= log2PRKM, fill=Sex)) + 
+  geom_boxplot(notch=TRUE)  +
+  ylim(-2,5) +
+  scale_fill_manual(values = c("red","blue")) +
+  theme(axis.title.x = element_text(size=15,colour = "black"),axis.title.y = element_text(size=15,colour = "black")) +
+  theme(axis.text.x = element_text(colour="black",size=12),axis.text.y = element_text(colour="black",size=12))
+
+gonad_unbias_rpkm <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_unbias_all2.txt", header = TRUE)
+str(gonad_unbias_rpkm)
+
+ggplot(data = gonad_unbias_rpkm, aes(x=Sex, y= log2RPKM, fill=Sex)) + 
+  geom_boxplot(notch=TRUE)  +
+  ylim(-2,5) +
+  scale_fill_manual(values = c("red","blue")) +
+  theme(axis.title.x = element_text(size=15,colour = "black"),axis.title.y = element_text(size=15,colour = "black")) +
+  theme(axis.text.x = element_text(colour="black",size=12),axis.text.y = element_text(colour="black",size=12))
+
+pdf(file="/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/output/figures/sbunbias_logrpkm_gonad.pdf")
+
+gonad_all_rpkm <- read.table("/Users/Wen-Juan/my_postdoc/useful_scripts/Rana_Transcriptome/input/CPM/gonad_all_rkpm_fi.txt", header = TRUE)
+str(gonad_all_rpkm)
+
+ggplot(data = gonad_all_rpkm, aes(x=Bias, y= log2RPKM, fill=Sex)) + 
+  geom_boxplot(notch=TRUE)  +
+  ylim(-2,5) +
+  scale_fill_manual(values = c("red","blue")) +
+  theme(axis.title.x = element_text(size=15,colour = "black"),axis.title.y = element_text(size=15,colour = "black")) +
+  theme(axis.text.x = element_text(colour="black",size=12),axis.text.y = element_text(colour="black",size=12))
 dev.off()
+
+
